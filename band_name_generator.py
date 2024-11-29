949425e13642dbba0353ed119f2bd74c6c0b3af9
Okoroafor Ebubechukwu Covenant <thisisebubechukwu@gmail.com>
2024-11-29 19:59:56 +0100

    Add files via upload

---
+++ band_name_generator.py
@@ -0,0 +1,5 @@
+# Welcome to the band name generator
+welcome_message = print("Welcome to the band name generator")
+city = input("Which city did you grow up in?").upper()
+pet = input("What is the name of your pet?").upper()
+band_name = print(f"The name of your band could be {city} {pet}.")
